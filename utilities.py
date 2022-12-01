e3a26a2f8e47fcc6d48861db5800c8476f9bbdb2
James Jia <32031865+wenzhaojia2000@users.noreply.github.com>
2022-12-01 09:59:50 +0000

    Writes skeleton functions

---
+++ utilities.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import numpy as np
+
+def read_file(path: Path) -> "np.array":
+    """
+    Reads a CSV file.
+    
+    Inputs
+    ------
+    Path: Path object
+
+    Returns
+    -------
+    np. array
+
+    """
+    pass
+
+def calculate_average(array: "np.array") -> "np.array":
+    """
+    Calculates the average over the columns.
+
+    Parameters
+    ----------
+    array : "np.array"
+
+    Returns
+    -------
+    np.array
+
+    """
+    pass
+
+def save_file(array: "np.array") -> None:
+    """
+    Saves the file to a csv file.
+
+    Parameters
+    ----------
+    array : "np.array"
+        DESCRIPTION.
+
+    Returns
+    -------
+    None
+
+    """
+    pass
